916b65c61498c7f4a7d3ed3e726db9180fdeaaeb
Groumphh <groumphh38@gmail.com>
2026-03-11 10:52:08 +0100

    Corrected paths

--- model/model.py
+++ model/model.py
@@ -1,9 +1,11 @@
-from mesa import Model
-from mesa.time import BaseScheduler
-from mesa.space import ContinuousSpace
-from components import Source, Sink, SourceSink, Bridge, Link, Intersection
-import pandas as pd
+import os
 from collections import defaultdict
+
+import pandas as pd
+from components import Bridge, Intersection, Link, Sink, Source, SourceSink
+from mesa import Model
+from mesa.space import ContinuousSpace
+from mesa.time import BaseScheduler
 
 
 # ---------------------------------------------------------------
@@ -55,7 +57,8 @@ class BangladeshModel(Model):
 
     step_time = 1
 
-    file_name = '../data/demo-4.csv'
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    file_name = os.path.join(BASE_DIR, "data", "demo-4.csv")
 
     def __init__(self, seed=None, x_max=500, y_max=500, x_min=0, y_min=0):
 
@@ -79,24 +82,24 @@ class BangladeshModel(Model):
 
         # a list of names of roads to be generated
         # TODO You can also read in the road column to generate this list automatically
-        roads = ['N1', 'N2']
+        roads = ["N1", "N2"]
 
         df_objects_all = []
         for road in roads:
             # Select all the objects on a particular road in the original order as in the cvs
-            df_objects_on_road = df[df['road'] == road]
+            df_objects_on_road = df[df["road"] == road]
 
             if not df_objects_on_road.empty:
                 df_objects_all.append(df_objects_on_road)
 
                 """
-                Set the path 
+                Set the path
                 1. get the serie of object IDs on a given road in the cvs in the original order
                 2. add the (straight) path to the path_ids_dict
                 3. put the path in reversed order and reindex
                 4. add the path to the path_ids_dict so that the vehicles can drive backwards too
                 """
-                path_ids = df_objects_on_road['id']
+                path_ids = df_objects_on_road["id"]
                 path_ids.reset_index(inplace=True, drop=True)
                 self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
                 self.path_ids_dict[path_ids[0], None] = path_ids
@@ -108,11 +111,7 @@ class BangladeshModel(Model):
         # put back to df with selected roads so that min and max and be easily calculated
         df = pd.concat(df_objects_all)
         y_min, y_max, x_min, x_max = set_lat_lon_bound(
-            df['lat'].min(),
-            df['lat'].max(),
-            df['lon'].min(),
-            df['lon'].max(),
-            0.05
+            df["lat"].min(), df["lat"].max(), df["lon"].min(), df["lon"].max(), 0.05
         )
 
         # ContinuousSpace from the Mesa package;
@@ -121,39 +120,49 @@ class BangladeshModel(Model):
 
         for df in df_objects_all:
             for _, row in df.iterrows():  # index, row in ...
-
                 # create agents according to model_type
-                model_type = row['model_type'].strip()
+                model_type = row["model_type"].strip()
                 agent = None
 
-                name = row['name']
+                name = row["name"]
                 if pd.isna(name):
                     name = ""
                 else:
                     name = name.strip()
 
-                if model_type == 'source':
-                    agent = Source(row['id'], self, row['length'], name, row['road'])
+                if model_type == "source":
+                    agent = Source(row["id"], self, row["length"], name, row["road"])
                     self.sources.append(agent.unique_id)
-                elif model_type == 'sink':
-                    agent = Sink(row['id'], self, row['length'], name, row['road'])
+                elif model_type == "sink":
+                    agent = Sink(row["id"], self, row["length"], name, row["road"])
                     self.sinks.append(agent.unique_id)
-                elif model_type == 'sourcesink':
-                    agent = SourceSink(row['id'], self, row['length'], name, row['road'])
+                elif model_type == "sourcesink":
+                    agent = SourceSink(
+                        row["id"], self, row["length"], name, row["road"]
+                    )
                     self.sources.append(agent.unique_id)
                     self.sinks.append(agent.unique_id)
-                elif model_type == 'bridge':
-                    agent = Bridge(row['id'], self, row['length'], name, row['road'], row['condition'])
-                elif model_type == 'link':
-                    agent = Link(row['id'], self, row['length'], name, row['road'])
-                elif model_type == 'intersection':
-                    if not row['id'] in self.schedule._agents:
-                        agent = Intersection(row['id'], self, row['length'], name, row['road'])
+                elif model_type == "bridge":
+                    agent = Bridge(
+                        row["id"],
+                        self,
+                        row["length"],
+                        name,
+                        row["road"],
+                        row["condition"],
+                    )
+                elif model_type == "link":
+                    agent = Link(row["id"], self, row["length"], name, row["road"])
+                elif model_type == "intersection":
+                    if not row["id"] in self.schedule._agents:
+                        agent = Intersection(
+                            row["id"], self, row["length"], name, row["road"]
+                        )
 
                 if agent:
                     self.schedule.add(agent)
-                    y = row['lat']
-                    x = row['lon']
+                    y = row["lat"]
+                    x = row["lon"]
                     self.space.place_agent(agent, (x, y))
                     agent.pos = (x, y)
 
@@ -183,5 +192,6 @@ class BangladeshModel(Model):
         Advance the simulation by one step.
         """
         self.schedule.step()
+
 
 # EOF -----------------------------------------------------------
