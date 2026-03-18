667ad221374482b536f9d340a3a355c0a0d11952
Isabela Aterman <materman@tudelft.nl>
2026-03-18 13:13:48 +0100

    found the lrp intersections

--- data/input_dataset_reformatting/data_prepare.py
+++ data/input_dataset_reformatting/data_prepare.py
@@ -1,10 +1,38 @@
+import os
 import pandas as pd
+from geopy.distance import geodesic
 
-df = pd.read_csv('_roads3.csv')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+file_name = os.path.join(BASE_DIR, "input_dataset_reformatting", "_roads3.csv")
+df = pd.read_csv(file_name)
 
-roads = ['N1', 'N2']
-target_roads = df.loc[(df['road'].str.startswith(('N1', 'N2'))) & (df['road'].str.len() == 4)]
-new_roads = target_roads['road'].unique().tolist()
-roads = list(set(roads + new_roads))
+crossings = [('N1', 'N101'), ('N1', 'N102'), ('N1', 'N104'), ('N1', 'N105'), ('N1', 'N106'), ('N1', 'N107'), ('N1', 'N108'), ('N1', 'N109'), ('N1', 'N110'), ('N1', 'N111')]
 
-print(roads)
+for road_a, road_b in crossings:
+    road_b_lrp_row = df[(df['lrp'] == 'LRPS') & (df['road'] == road_b)]
+    target_lat = road_b_lrp_row['lat'].iloc[0]
+    target_lon = road_b_lrp_row['lon'].iloc[0]
+
+    road_a_lrps = df.loc[df['road'] == road_a]
+
+    approx_limit = 0.001
+    limit_meters = 100
+    target_coords = (target_lat, target_lon)
+
+    close_candidates = road_a_lrps[
+        (road_a_lrps['lat'].between(target_coords[0] - approx_limit, target_coords[0] + approx_limit)) &
+        (road_a_lrps['lon'].between(target_coords[1] - approx_limit, target_coords[1] + approx_limit))
+        ]
+
+    for index, row in close_candidates.iterrows():
+        current_coords = (row['lat'], row['lon'])
+
+        # Calculate exact geodesic distance
+        dist = geodesic(target_coords, current_coords).meters
+
+        if dist < limit_meters:
+            # this is our intersection
+            intersection = row
+            print(f'Intersection between {road_a} and {road_b}  is at  {intersection}')
+            break
+
